3423b316f48c01e92dad144fc45e4406f6d37bca
tanmayjadhav <tanmayjadhav205@gmail.com>
2026-01-31 19:13:02 +0530

    Data Types in PYthon

---
+++ python complete/variables/Datatypes/datatype1.py
@@ -0,0 +1,14 @@
+
+a = 5  #integer datatype
+b = 5.5  #float datatype
+c = "Hello"  #string datatype
+d = "World" #string datatype
+e = True  #boolean datatype
+f = None   #NoneType datatype
+
+print(type(a)) 
+print(type(b))  
+print(type(c))  
+print(type(d))  
+print(type(e))  
+print (type(f))  
