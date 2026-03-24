5617455ee468519bbf2a378215193274c3002531
tanmayjadhav <tanmayjadhav205@gmail.com>
2026-03-25 00:16:21 +0530

    daily problem 4new

--- python complete/DailyPracticeQue/daily4.py
+++ python complete/DailyPracticeQue/daily4.py
@@ -2,11 +2,9 @@ a = input ("Enter a first number: ")
 b = input ("Enter a second number: ")
 
 if int(a) > int(b):
-    print ("a is greater than b")
+    print ("true")
 
-elif int(a) == int(b):
-    print ("a is equal to b")
         
 else:
-    print("b is greater than a")
+    print("false")
     
