2d88b4acd852d37912fd88e522ffc547b63d6154
tanmayjadhav <tanmayjadhav205@gmail.com>
2026-03-31 18:56:29 +0530

    Fuctions in String

---
+++ python complete/DailyPracticeQue/Strings/stringfunctn.py
@@ -0,0 +1,26 @@
+#(1)
+#  # str = "Tanmay"
+# print(str.endswith("ay"))     #endswith function
+
+
+# (2) 
+# str = "tanmay" 
+# print (str.capitalize())     # capitalize function
+
+
+#(3)
+# str = "Tanmay Jadhav"
+# print(str.replace("Tanmay", "khushi"))   #replace function
+
+
+# #(4)
+# str="tanmay" 
+# print(str.find("m"))   #find function
+
+#(5)
+str = "tanmay"
+print(str.count("a")) 
+
+
+
+
