2c572d95fab1f22803d8f84b77ac5d256c455396
tanmayjadhav <tanmayjadhav205@gmail.com>
2026-04-08 21:44:47 +0530

    if-else statement

--- python complete/conditional statement/if-elif-else.py
+++ python complete/conditional statement/if-elif-else.py
@@ -1,11 +1,18 @@
-light= "green"
+# light= "purple"
 
-if (light == "red"):
-    print("stop")
-elif (light == "green"):
-    print("go")
+# if (light == "red"):
+#     print("stop")
+# elif (light == "green"):
+#     print("go")
 
-elif (light == "yellow"):
-    print("wait")
+# elif (light == "yellow"):
+#     print("wait")
+# else:
+#     print("invalid light")
 
+age = 22
+if(age>=18):
+   print("You can vote")
+else:
+   print("You cannot vote")
 
