1e2ae6b92b6396bbf09b24d2828a6f3e2de1a50c
tanmayjadhav <tanmayjadhav205@gmail.com>
2026-04-08 22:04:53 +0530

    grades of students

---
+++ python complete/conditional statement/gradeformarks.py
@@ -0,0 +1,10 @@
+marks = int(input("Enter your marks:"))
+
+if (marks >= 90):
+    print("grade A")
+elif(marks >= 80 and marks <= 90):
+    print("grade B")
+elif(marks >= 70 and marks <= 80):
+    print("grade C")
+else:
+    print("You are fail")
--- python complete/conditional statement/if-elif-else.py
+++ python complete/conditional statement/if-elif-else.py
@@ -10,9 +10,9 @@
 # else:
 #     print("invalid light")
 
-age = 22
+age = 18
 if(age>=18):
-   print("You can vote")
+   print("You can vote")  #indentation is very important in python, (4 spaces or 1 tab before the print statement)
 else:
    print("You cannot vote")
 
