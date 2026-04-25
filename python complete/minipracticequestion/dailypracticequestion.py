5c48c36049083a08a233e090fb7c94480b749a32
tanmayjadhav <tanmayjadhav205@gmail.com>
2026-04-25 20:54:17 +0530

    even odd 2nd meathod

--- python complete/minipracticequestion/dailypracticequestion.py
+++ python complete/minipracticequestion/dailypracticequestion.py
@@ -1,7 +1,14 @@
-# WAP to check whether the input number is even or odd
-num = int(input("Enter the number"))
-rem = num % 2
-if(num ==0):
+# # WAP to check whether the input number is even or odd
+# num = int(input("Enter the number"))
+# rem = num % 2
+# if(num ==0):
+#     print("Even")
+# else:
+#  print("Odd")
+
+num = int(input("Enter the number "))
+
+if( num % 2 == 0):
     print("Even")
 else:
- print("Odd")
+    print("Odd")
