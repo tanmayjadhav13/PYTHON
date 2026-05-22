92a586ed143a9ccab59ba0224a24a416f457551e
tanmayjadhav <tanmayjadhav205@gmail.com>
2026-05-22 19:18:29 +0530

    Tuples

---
+++ python complete/Tuples/tuples.py
@@ -0,0 +1,16 @@
+# tup = (1,4,7)
+# print(tup[0])
+
+#Empty tuple
+tup =()
+print(tup)
+
+#tuple with one element
+tup = (1)
+print(tup)
+print(type(tup))
+
+#Element with tuple
+tup = (1,3,5,7,9,0)
+print(tup)
+print(tup[0])
